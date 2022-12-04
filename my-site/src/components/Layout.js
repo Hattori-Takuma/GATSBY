67c767614bf2094c7168b0e99d62a0cd782889ca
Hattori-Takuma <hattori.ta@icloud.com>
2022-12-04 16:22:41 +0900

    component

---
+++ my-site/src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../styles/style.css"
+import Header from "./header"
+import Footer from "./footer"
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
